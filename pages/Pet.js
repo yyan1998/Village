cd60428c814f64f05327cd9982f488ffc0d09856
DEHAO TU <dehaotu@DEHAOs-MacBook-Pro.local>
2021-06-04 07:04:52 -0700

    pet

--- pages/Pet.js
+++ pages/Pet.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {PetViewer} from './../scripts/petViewer.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Pet</Text>
+      <PetViewer />
       <StatusBar style="auto" />
     </View>
   );
